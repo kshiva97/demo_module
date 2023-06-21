07c1c072f49d9225fbd5d6953f8572624a3b0715
shivaraj <kshivaraj1997@gmail.com>
2023-06-21 10:55:03 +0530

    initial files

---
+++ infra/modules/vm/main.tf
@@ -0,0 +1,13 @@
+resource "google_compute_instance" "vm" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
---
+++ infra/modules/vm/provider.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  project = "terraform-project-382714"
+  credentials = file("~/Downloads/terraform-project.json")
+}
